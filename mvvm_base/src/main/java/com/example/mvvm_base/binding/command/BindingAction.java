93737bd4f0596b72466798707e922c97501bf9af
bWang <wb@iotcnn.com>
2021-07-01 16:10:43 +0800

    BindingCommand add

---
+++ mvvm_base/src/main/java/com/example/mvvm_base/binding/command/BindingAction.java
@@ -0,0 +1,9 @@
+package com.example.mvvm_base.binding.command;
+
+/**
+ * A zero-argument action.
+ */
+
+public interface BindingAction {
+    void call();
+}
---
+++ mvvm_base/src/main/java/com/example/mvvm_base/binding/command/BindingFunction.java
@@ -0,0 +1,10 @@
+package com.example.mvvm_base.binding.command;
+
+/**
+ * Represents a function with zero arguments.
+ *
+ * @param <T> the result type
+ */
+public interface BindingFunction<T> {
+    T call();
+}
