ad968a0b7e90118508b5e49bfabc8e8e2bcddbdb
bWang <wb@iotcnn.com>
2021-07-01 17:17:31 +0800

    ViewAdapter add

--- mvvm_base/src/main/java/com/example/mvvm_base/binding/viewadapter/swiperefresh/ViewAdapter.java
+++ mvvm_base/src/main/java/com/example/mvvm_base/binding/viewadapter/swiperefresh/ViewAdapter.java
@@ -1,9 +1,14 @@
 package com.example.mvvm_base.binding.viewadapter.swiperefresh;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.BindingAdapter;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.mvvm_base.binding.command.BindingCommand;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.RefreshState;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 
 /**
@@ -12,21 +17,31 @@ import com.example.mvvm_base.binding.command.BindingCommand;
 public class ViewAdapter {
     //下拉刷新命令
     @BindingAdapter({"onRefreshCommand"})
-    public static void onRefreshCommand(SwipeRefreshLayout swipeRefreshLayout, final BindingCommand onRefreshCommand) {
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+    public static void onRefreshCommand(SmartRefreshLayout smartRefreshLayout, final BindingCommand onRefreshCommand) {
+        smartRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
-            public void onRefresh() {
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
                 if (onRefreshCommand != null) {
                     onRefreshCommand.execute();
                 }
+                refreshLayout.finishRefresh(smartRefreshLayout.getState().equals(RefreshState.RefreshFinish));
             }
         });
     }
 
-    //是否刷新中
-    @BindingAdapter({"refreshing"})
-    public static void setRefreshing(SwipeRefreshLayout swipeRefreshLayout, boolean refreshing) {
-        swipeRefreshLayout.setRefreshing(refreshing);
+
+    //上拉加载命令
+    @BindingAdapter({"onLoadMoreCommand"})
+    public static void onLoadMoreCommand(SmartRefreshLayout smartRefreshLayout, final BindingCommand onLoadMoreCommand) {
+        smartRefreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                if (onLoadMoreCommand != null) {
+                    onLoadMoreCommand.execute();
+                }
+                refreshLayout.finishLoadMore(smartRefreshLayout.getState().equals(RefreshState.LoadFinish));
+            }
+        });
     }
 
 }
