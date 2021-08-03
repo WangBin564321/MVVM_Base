88929441de4cf9ecdcc2d1ec4b1eb8e9ad6ea193
bWang <wb@iotcnn.com>
2021-08-03 14:39:30 +0800

    some update

---
+++ mvvm_base/src/main/java/com/example/mvvm_base/binding/viewadapter/textview/ViewAdapter.java
@@ -0,0 +1,28 @@
+package com.example.mvvm_base.binding.viewadapter.textview;
+
+import android.os.Build;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+/**
+ * desc:
+ * date:2021/8/3
+ * author:bWang
+ * <p>
+ */
+public class ViewAdapter {
+
+    /**
+     * 背景
+     */
+    @BindingAdapter(value = {"txtColor"}, requireAll = false)
+    public static void setBg(TextView textView, int colorRes) {
+        if (colorRes != 0)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textView.setTextColor(textView.getContext().getColor(colorRes));
+            }
+
+    }
+}
